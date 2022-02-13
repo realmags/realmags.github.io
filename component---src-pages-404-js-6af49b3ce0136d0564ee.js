48cde599c6041ee15aa7aaa1715ce82479b4da5a
realmags <kentbryanmaglinao9@gmail.com>
2022-02-13 20:12:23 +0800

    Updates

---
+++ component---src-pages-404-js-6af49b3ce0136d0564ee.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,n){n.r(e);var u=n(7294),a=n(5987),l=n(9376);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6af49b3ce0136d0564ee.js.map
---
+++ component---src-pages-page-2-js-1d23879daf46ec9e2157.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(5987),c=a(9376);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-1d23879daf46ec9e2157.js.map
